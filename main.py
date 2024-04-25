e8744bbd9241958001032ce478078cc5bdb70c7e
Maks6666 <maxim.kucher2005@gmail.com>
2024-04-25 20:46:26 +0300

    2nd task

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# Розроблення програми з таймером, що підраховує
-# час. Використати JSON для збереження стану таймера
-# (наприклад, поточний час) у файлі. При перезапуску
-# програми відновити час збереженого стану за
-# допомогою завантаження даних з JSON-файлу.
+# Реалізація програми для додавання, видалення та
+# відстеження завдань/заміток. Зберігати ці завдання у
+# форматі JSON у файлі. Можливість завантаження
+# раніше збережених завдань для подальшої роботи з
+# ними.
 
 import time
 import json
@@ -10,24 +10,53 @@ import json
 current_time = time.localtime()
 
 
-class Time():
-    def __init__(self):
-        self.hours = current_time.tm_hour
-        self.minutes = current_time.tm_min
+class Tasks():
+    def __init__(self, lst):
+        self.lst = lst
 
-    def get_time(self):
-        return f"{self.hours}:{self.minutes}"
+    def get_tasks(self):
+        return f"Tasks: {self.lst}"
 
-    def save(self, filename='timer.json'):
-        dct = {"hours": self.hours,
-               "minutes": self.minutes}
+    def add_tasks(self, filename='tasks.json'):
+        number_of_tasks = int(input("How many tasks do you want to add? "))
+        for i in range(number_of_tasks):
+            task = input("Input task name: ")
+            self.lst.append(task)
+
+        dct = {"list_of_tasks": self.lst}
         with open(filename, 'w') as file:
             json.dump(dct, file)
 
+    def del_tasks(self, filename='tasks.json'):
+        number_of_tasks = int(input("How many tasks do you want to delete? "))
+        for i in range(number_of_tasks):
+            self.lst.pop()
+        dct = {"list_of_tasks": self.lst}
+        with open(filename, 'w') as file:
+            json.dump(dct, file)
 
-timer = Time()
-print(timer.get_time())
-timer.save()
+    def read_tasks(self, filename='tasks.json'):
+        with open(filename, 'r') as file:
+            dct = json.load(file)
+        self.lst = dct['list_of_tasks']
+        return self.lst
+
+
+    def is_task_in_list(self, task):
+        self.task = task
+        if self.task in  self.lst:
+            return True
+        else:
+            return False
+
+tasks = Tasks(["task_1", "task_2", "task_3", "task_4"])
+print(tasks.get_tasks())
+tasks.add_tasks()
+tasks.del_tasks()
+print(tasks.read_tasks())
+print(tasks.is_task_in_list("task_1"))
+print(tasks.is_task_in_list("t"))
+
 
 
 
